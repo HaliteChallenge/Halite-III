519e38f66c497c831225df86831bae673394ba3d
Charles Yuan <charlesy@twosigma.com>
2018-06-06 23:03:18 -0400

    add new configuration section

---
+++ environment/config/Config.hpp
@@ -0,0 +1,19 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+namespace hlt {
+
+/** Global configuration. */
+struct Config {
+    int id;
+    unsigned int seed;
+    bool quiet_output;
+    bool always_log;
+    bool ignore_timeout;
+    bool enable_replay;
+    bool enable_compression;
+};
+
+}
+
+#endif // CONFIG_HPP
