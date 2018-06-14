e5a5dec8423594c567f214d2c2d45721e77d05e6
Charles Yuan <charlesyuan314@gmail.com>
2018-06-14 15:05:45 -0400

    autoformat mapgen/ (whitespace only)

--- environment/mapgen/BlurTileGenerator.cpp
+++ environment/mapgen/BlurTileGenerator.cpp
@@ -1,83 +1,83 @@
 #include "BlurTileGenerator.hpp"
-#include "Map.hpp"
-#include "Constants.hpp"
 
 namespace hlt {
-    namespace mapgen {
-        // Blur function takes in map, and a cell, and calculates a new production value for that cell
-        // based on some blurring with surrounding cells
-        // currently blurs entire map, not just a tile
-        long BlurTileGenerator::blur_function(hlt::dimension_type y_coord, hlt::dimension_type x_coord, const hlt::Map &map) const {
-            // bring into local scope for shorter naming
-            const auto BLUR_FACTOR = hlt::GameConstants::get().BLUR_FACTOR;
-            // Weight of a neighbor's effect on a cell's production value is dependent on the number of neighbors being considered
-            // Declare as local constant as rest of function code is also implicitly dependent on the number of neighbors
-            const auto NUM_NEIGHBORS = 4;
+namespace mapgen {
+// Blur function takes in map, and a cell, and calculates a new production value for that cell
+// based on some blurring with surrounding cells
+// currently blurs entire map, not just a tile
+long
+BlurTileGenerator::blur_function(hlt::dimension_type y_coord, hlt::dimension_type x_coord, const hlt::Map &map) const {
+    // bring into local scope for shorter naming
+    const auto BLUR_FACTOR = hlt::GameConstants::get().BLUR_FACTOR;
+    // Weight of a neighbor's effect on a cell's production value is dependent on the number of neighbors being considered
+    // Declare as local constant as rest of function code is also implicitly dependent on the number of neighbors
+    const auto NUM_NEIGHBORS = 4;
 
-            // Blur function looks only at immediate neighbors of a tile, wrapping around the edges of the tile as needed
-            long left_coord = (x_coord - 1 + tile_width) % tile_width;
-            long right_coord = (x_coord + 1) % tile_width;
-            long above_coord = (y_coord - 1 + tile_height) % tile_height;
-            long below_coord = (y_coord + 1) % tile_height;
+    // Blur function looks only at immediate neighbors of a tile, wrapping around the edges of the tile as needed
+    long left_coord = (x_coord - 1 + tile_width) % tile_width;
+    long right_coord = (x_coord + 1) % tile_width;
+    long above_coord = (y_coord - 1 + tile_height) % tile_height;
+    long below_coord = (y_coord + 1) % tile_height;
 
-            // In determining post blur production value, give current production value of cell weight BLUR_FACTOR
-            // and production of neighbors weight (1 - BLUR_FACTOR)
-            // This means that *each* neighbor's production gets weight (1 - BLUR_FACTOR) / 4
-            // Truncate fractions via casting to long
-            auto new_production = (long) (map.grid[y_coord][x_coord] -> production() * BLUR_FACTOR
-                                          + map.grid[y_coord][left_coord] -> production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
-                                          + map.grid[y_coord][right_coord]-> production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
-                                          + map.grid[above_coord][x_coord] -> production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
-                                          + map.grid[below_coord][x_coord] -> production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS);
-            return new_production;
+    // In determining post blur production value, give current production value of cell weight BLUR_FACTOR
+    // and production of neighbors weight (1 - BLUR_FACTOR)
+    // This means that *each* neighbor's production gets weight (1 - BLUR_FACTOR) / 4
+    // Truncate fractions via casting to long
+    auto new_production = (long) (map.grid[y_coord][x_coord]->production() * BLUR_FACTOR
+                                  + map.grid[y_coord][left_coord]->production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
+                                  + map.grid[y_coord][right_coord]->production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
+                                  + map.grid[above_coord][x_coord]->production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS
+                                  + map.grid[below_coord][x_coord]->production() * (1 - BLUR_FACTOR) / NUM_NEIGHBORS);
+    return new_production;
+}
+
+hlt::Map BlurTileGenerator::generate(std::list<hlt::Player> &players) {
+
+    auto tile = Map(tile_width, tile_height);
+
+    // Fetch max and min values for square production and store for ease of use
+    const auto MIN_CELL_PROD = hlt::GameConstants::get().MIN_CELL_PRODUCTION;
+    const auto MAX_CELL_PROD = hlt::GameConstants::get().MAX_CELL_PRODUCTION;
+
+    for (auto row = 0; row < tile_height; ++row) {
+        for (auto col = 0; col < tile_width; ++col) {
+            // randomly generate a production value using generator class' random number generator
+            auto production = (unsigned long) (rng() / (double) std::mt19937::max() * (MAX_CELL_PROD - MIN_CELL_PROD) +
+                                               MIN_CELL_PROD);
+            tile.grid[row][col] = make_cell<NormalCell>(production);
         }
-
-        hlt::Map BlurTileGenerator::generate(std::list<hlt::Player> &players) {
-
-            auto tile = Map(tile_width, tile_height);
-
-            // Fetch max and min values for square production and store for ease of use
-            const auto MIN_CELL_PROD = hlt::GameConstants::get().MIN_CELL_PRODUCTION;
-            const auto MAX_CELL_PROD = hlt::GameConstants::get().MAX_CELL_PRODUCTION;
-
-            for (auto row = 0; row < tile_height; ++row) {
-                for (auto col = 0; col < tile_width; ++col) {
-                    // randomly generate a production value using generator class' random number generator
-                    auto production = (unsigned long) (rng() / (double) std::mt19937::max() * (MAX_CELL_PROD - MIN_CELL_PROD) + MIN_CELL_PROD);
-                    tile.grid[row][col] = make_cell<NormalCell>(production);
-                }
-            }
-
-            // For each cell, use the blur function to determine new production value for cell based on neighboring cell values
-            // Blurring will create regression to mean, so additionally keep track of max value to later ensure full use of
-            // production range
-            long max_seen_prod = 0;
-            for (auto row = 0; row < tile_height; ++row) {
-                for (auto col = 0; col < tile_width; ++col) {
-                    // production is a private member, so create new cell with new production value (rather than updating prod values directly)
-                    const long post_blur_prod = blur_function(row, col, tile);
-                    if (post_blur_prod > max_seen_prod) max_seen_prod = post_blur_prod;
-                    tile.grid[row][col] = make_cell<NormalCell>(post_blur_prod);
-                }
-            }
-
-             // Do a second pass over the tile to scale relative to post blur production, but to still get full range of
-             // production values
-            for (auto row = 0; row < tile_height; ++row) {
-                for (auto col = 0; col < tile_width; ++col) {
-                    const long post_normalized_prod = tile.grid[row][col]->production() * MAX_CELL_PROD / max_seen_prod;
-                    tile.grid[row][col] = make_cell<NormalCell>(post_normalized_prod);
-                }
-            }
-
-            const auto factory_pos_x = (hlt::dimension_type) (rng() / (float) std::mt19937::max() * tile_width);
-            const auto factory_pos_y = (hlt::dimension_type) (rng() / (float) std::mt19937::max() * tile_height);
-
-            // Use super class function to copy the tile over the entire map, including placing all factories
-            hlt::Map final_map = tile_map(factory_pos_y, factory_pos_x, tile, players);
-            return final_map;
-
-        }
-
     }
+
+    // For each cell, use the blur function to determine new production value for cell based on neighboring cell values
+    // Blurring will create regression to mean, so additionally keep track of max value to later ensure full use of
+    // production range
+    long max_seen_prod = 0;
+    for (auto row = 0; row < tile_height; ++row) {
+        for (auto col = 0; col < tile_width; ++col) {
+            // production is a private member, so create new cell with new production value (rather than updating prod values directly)
+            const long post_blur_prod = blur_function(row, col, tile);
+            if (post_blur_prod > max_seen_prod) max_seen_prod = post_blur_prod;
+            tile.grid[row][col] = make_cell<NormalCell>(post_blur_prod);
+        }
+    }
+
+    // Do a second pass over the tile to scale relative to post blur production, but to still get full range of
+    // production values
+    for (auto row = 0; row < tile_height; ++row) {
+        for (auto col = 0; col < tile_width; ++col) {
+            const long post_normalized_prod = tile.grid[row][col]->production() * MAX_CELL_PROD / max_seen_prod;
+            tile.grid[row][col] = make_cell<NormalCell>(post_normalized_prod);
+        }
+    }
+
+    const auto factory_pos_x = (hlt::dimension_type) (rng() / (float) std::mt19937::max() * tile_width);
+    const auto factory_pos_y = (hlt::dimension_type) (rng() / (float) std::mt19937::max() * tile_height);
+
+    // Use super class function to copy the tile over the entire map, including placing all factories
+    hlt::Map final_map = tile_map(factory_pos_y, factory_pos_x, tile, players);
+    return final_map;
+
+}
+
+}
 }
--- environment/mapgen/BlurTileGenerator.hpp
+++ environment/mapgen/BlurTileGenerator.hpp
@@ -6,43 +6,44 @@
 
 
 namespace hlt {
-    namespace mapgen {
-        /**
-         * Map generator using a "tiling" method. This generates a single tile of a map, and then duplicates
-         * that tile for each player, arranging the tiles into an appropriate number of rows and columns.
-         * The size of the tile is determined from the desired total height and width of the map,
-         * and the number of players.
-         */
-        class BlurTileGenerator : protected TileGenerator {
-        private:
-            /**
-             * Generate a production value correlated with neighboring cells
-             * @param y_coord: y_coordinate of cell of interest
-             * @param x_coord: x_coordinate of cell of interest
-             * @param map: map of the cells. Assumes map is at least as large as tile_width x tile_height
-             * @return new production value for the cell of interest
-             */
-            long blur_function(hlt::dimension_type y_coord, hlt::dimension_type x_coord, const hlt::Map &map) const;
-        public:
-            std::string name() const override  { return "Blur Tile"; };
+namespace mapgen {
+/**
+ * Map generator using a "tiling" method. This generates a single tile of a map, and then duplicates
+ * that tile for each player, arranging the tiles into an appropriate number of rows and columns.
+ * The size of the tile is determined from the desired total height and width of the map,
+ * and the number of players.
+ */
+class BlurTileGenerator : protected TileGenerator {
+private:
+    /**
+     * Generate a production value correlated with neighboring cells
+     * @param y_coord: y_coordinate of cell of interest
+     * @param x_coord: x_coordinate of cell of interest
+     * @param map: map of the cells. Assumes map is at least as large as tile_width x tile_height
+     * @return new production value for the cell of interest
+     */
+    long blur_function(hlt::dimension_type y_coord, hlt::dimension_type x_coord, const hlt::Map &map) const;
 
-            /** Generate a map given a list of players
-             *
-             * @param players: list of players of the game. Passed in for the purpose
-             *      of assigning each player's factory to a cell
-             * @return map with cells initialized (ie given production values, or
-             *      assigned to alternate cell type)
-             */
-            hlt::Map generate(std::list<hlt::Player> &players) override;
+public:
+    std::string name() const override { return "Blur Tile"; };
 
-            /**
-             * Construct BlurTileGenerator from parameters.
-             * @param parameters: The map generation parameters.
-             */
-            explicit BlurTileGenerator(const MapParameters &parameters) :
-                    TileGenerator(parameters) {};
-        };
-    }
+    /** Generate a map given a list of players
+     *
+     * @param players: list of players of the game. Passed in for the purpose
+     *      of assigning each player's factory to a cell
+     * @return map with cells initialized (ie given production values, or
+     *      assigned to alternate cell type)
+     */
+    hlt::Map generate(std::list<hlt::Player> &players) override;
+
+    /**
+     * Construct BlurTileGenerator from parameters.
+     * @param parameters: The map generation parameters.
+     */
+    explicit BlurTileGenerator(const MapParameters &parameters) :
+            TileGenerator(parameters) {};
+};
+}
 }
 
 #endif // BLURTILEGENERATOR_H
