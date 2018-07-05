f09278509e580d25966eebebb17d17f446c68c8b
Benjamin Spector <benjaminfspector@gmail.com>
2018-07-05 15:09:51 -0400

    Updated python starter kit for spawning

--- airesources/Python3/MyBot.py
+++ airesources/Python3/MyBot.py
@@ -7,4 +7,4 @@ hlt.send_init("MyPythonBot-"+str(myID))
 while True:
 	turn_number, players = hlt.get_frame()
 	moves = { location: random.choice(["n", "e", "s", "w"]) for location in players[myID].entities.keys() }
-	hlt.send_frame(moves)
+	hlt.send_frame(moves, 255 if players[myID].energy >= 255000 else 0)
