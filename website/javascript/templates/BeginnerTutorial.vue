a22ce054f490f8f79dd2ccd886e6c6c90b0b08f7
David Li <David.M.Li@TwoSigma.com>
2018-07-17 15:53:24 -0400

    Fill out beginner tutorial

--- website/javascript/templates/BeginnerTutorial.vue
+++ website/javascript/templates/BeginnerTutorial.vue
@@ -18,6 +18,25 @@
                     </walkthrough-substep>
                 </walkthrough-substeps>
             </Step>
+            <Step title="Time" name="time">
+                <p>
+                    The game progresses in turns: each turn, every
+                    player is sent the current game state, they issue
+                    commands, which are then processed. This repeats
+                    for 300 turns.
+                </p>
+                <walkthrough-substeps>
+                    <walkthrough-substep name="seas-pan">
+                        Drag the map around and observe the wrapping.
+                    </walkthrough-substep>
+                    <walkthrough-substep name="seas-zoom">
+                        Zoom in to examine the map more closely.
+                    </walkthrough-substep>
+                    <walkthrough-substep name="seas-reset">
+                        Reset the map to its initial state.
+                    </walkthrough-substep>
+                </walkthrough-substeps>
+            </Step>
             <Step title="Saltwater" name="salt">
                 <p>
                     In these seas are vast quantities of the material
@@ -36,6 +55,14 @@
                     much halite as possible.
                 </p>
             </Step>
+            <Step title="Shipyard" name="shipyard">
+                <p>
+                    Every player has a base of operations, called
+                    their shipyard. This stores the halite you
+                    collect, and turns halite into new ships for you
+                    to use.
+                </p>
+            </Step>
             <Step title="A Vessel" name="vessel">
                 <p>
                     Each vessel has a certain amount of halite, which
@@ -51,6 +78,56 @@
                         Click on a ship and observe how much halite it has.
                     </walkthrough-substep>
                 </walkthrough-substeps>
+            </Step>
+            <Step title="Collecting Halite" name="collection">
+                <p>
+                    When ships are not moving, they can collect halite
+                    from the sea by filtering it out of the
+                    water. Ships collect 10% of the halite present in
+                    the waters around them every turn.
+                </p>
+                <walkthrough-substeps>
+                    <walkthrough-substep>
+                        Compare the cargo on the two ships as they
+                        collect halite for a few turns.
+                    </walkthrough-substep>
+                </walkthrough-substeps>
+            </Step>
+            <Step title="Depositing Halite" name="depositing">
+                <p>
+                    This halite is not safe until it has been
+                    deposited in the shipyard. Ships may dump halite
+                    overboard; if they do so while on top of their
+                    shipyard, that halite will get collected by the
+                    player. Otherwise, it returns to the sea.
+                </p>
+            </Step>
+            <Step title="Drop-Off Areas" name="dropoffs">
+                <p>
+                    Bringing halite from the far corners of the globe
+                    can be perilous and slow. Instead, ships may build
+                    drop-off points anywhere on the map, for the cost
+                    of quite a bit of halite. Then, instead of
+                    returning to the shipyard, ships can simply
+                    deposit halite at the nearest drop-off.
+                </p>
+            </Step>
+            <Step title="Advanced" name="advanced">
+                <p>
+                    Drop-off points are not exclusive to the player
+                    who built them: other players may use them to
+                    deposit halite into their own accounts. Like an
+                    ATM, however, they charge hefty fees; a player who
+                    deposits halite at another's drop-off will find
+                    25% of the cargo redirected into the original
+                    player's accounts.
+                </p>
+                <p>
+                    Ships may not pass through each other. Two ships
+                    that try to enter the same cell at the same time
+                    will both be destroyed; their cargo will be
+                    deposited where they collided.
+                </p>
             </Step>
         </template>
         <template slot="content" slot-scope="walkthrough">
