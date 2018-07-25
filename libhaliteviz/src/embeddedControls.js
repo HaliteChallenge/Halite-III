95f6ce95fe141755985984559eba1be663d4aa68
David Li <David.M.Li@TwoSigma.com>
2018-07-25 14:23:23 -0400

    Add visualization of relative player energy to embedded visualizer

--- libhaliteviz/src/embeddedControls.js
+++ libhaliteviz/src/embeddedControls.js
@@ -14,6 +14,24 @@ const css = `
     color: #FFF;
 }
 
+.embedded-clashbar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    opacity: 0;
+    height: 1em;
+    transition: opacity 0.3s ease-in;
+    display: flex;
+    cursor: help;
+}
+
+.embedded-clashbar-energybar {
+    height: 1em;
+    opacity: 0.5;
+    transition: width 0.2s ease;
+}
+
 .embedded-toolbar {
     position: absolute;
     bottom: 0;
@@ -33,7 +51,9 @@ const css = `
 }
 
 .embedded-visualizer:hover .embedded-toolbar {
-    display: block;
+    opacity: 1;
+}
+.embedded-visualizer:hover .embedded-clashbar {
     opacity: 1;
 }
 `;
@@ -53,6 +73,17 @@ export default class EmbeddedVisualizer extends HaliteVisualizer {
         container.classList.add("embedded-visualizer");
 
         super.attach(realContainer, $(containerEl)[0]);
+
+        // Display of relative energy stores
+        const clashbar = document.createElement("div");
+        clashbar.classList.add("embedded-clashbar");
+
+        for (let i = 0; i < this.replay.players.length; i++) {
+            const bar = document.createElement("div");
+            bar.classList.add(`bg-player-${i+1}`);
+            bar.classList.add("embedded-clashbar-energybar");
+            clashbar.appendChild(bar);
+        }
 
         const toolbar = document.createElement("div");
         toolbar.classList.add("embedded-toolbar");
@@ -79,6 +110,39 @@ export default class EmbeddedVisualizer extends HaliteVisualizer {
         });
         this.onUpdate.add(() => {
             progress.innerText = `${this.frame}/${this.replay.full_frames.length - 1}`;
+
+            if (!this.currentFrame) return;
+
+            const energies = this.currentFrame.energy;
+            const widths = {};
+            let total = 0;
+            let usedWidth = 0;
+            for (const [ id, energy ] of Object.entries(energies)) {
+                total += energy;
+                if (energy === 0) {
+                    widths[id] = 10;
+                    usedWidth += 10;
+                }
+            }
+            for (const [ id, energy ] of Object.entries(energies)) {
+                if (typeof widths[id] === "undefined") {
+                    widths[id] = (energy / total) * (100 - usedWidth);
+                }
+                else if (total === 0) {
+                    widths[id] = 100 / Object.keys(energies).length;
+                }
+            }
+            for (const [ id, width ] of Object.entries(widths)) {
+                clashbar
+                    .querySelector(`div:nth-child(${parseInt(id, 10) + 1})`)
+                    .style.width = `${width}%`;
+            }
+
+            const tooltip = [];
+            for (let i = 0; i < this.replay.players.length; i++) {
+                tooltip.push(`Player ${i}: ${energies[i]} halite stored.`);
+            }
+            clashbar.setAttribute("title", tooltip.join(" "));
         });
 
         play.addEventListener("click", () => {
@@ -98,6 +162,7 @@ export default class EmbeddedVisualizer extends HaliteVisualizer {
             this.playSpeed = Math.min(20, this.playSpeed + 1);
         });
 
+        container.appendChild(clashbar);
         container.appendChild(toolbar);
 
         if (!injectedCSS) {
@@ -106,5 +171,17 @@ export default class EmbeddedVisualizer extends HaliteVisualizer {
             document.body.appendChild(style);
             injectedCSS = true;
         }
+
+        this.container = container;
+    }
+
+    destroy() {
+        super.destroy();
+
+        for (const node of this.container.querySelectorAll("*")) {
+            node.remove();
+        }
+
+        delete this.container;
     }
 }
