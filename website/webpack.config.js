c0b57b229a9b05ef4196f7abea9575311621f563
David Li <David.M.Li@TwoSigma.com>
2018-07-27 09:12:48 -0400

    Add Webpack bundle analyzer

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,6 +1,10 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
     devtool: "source-map",
+    plugins: [
+        new BundleAnalyzerPlugin(),
+    ],
 });
