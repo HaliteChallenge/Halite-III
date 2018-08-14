652bba16ac3d985308a045e0de50431dbec9aa20
David Li <David.M.Li@TwoSigma.com>
2018-08-14 10:45:40 -0400

    Increase default window size (#160)

--- tools/halite-in-a-box/index.js
+++ tools/halite-in-a-box/index.js
@@ -6,7 +6,7 @@ let win;
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600});
+    win = new BrowserWindow({width: 1024, height: 640});
 
     // and load the index.html of the app.
     win.loadFile("index.html");
