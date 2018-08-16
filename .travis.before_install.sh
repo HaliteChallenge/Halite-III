2e15d06ce895cf37b12c315a3abb34bf681e89e2
David Li <David.M.Li@TwoSigma.com>
2018-08-16 09:49:22 -0400

    Try #2 to add MacOS to Travis

---
+++ .travis.before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+env
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo ""
+else
+    docker pull ubuntu:bionic
+    docker run -itd --name build -v $(pwd):/project ubuntu:bionic /bin/bash
+    docker exec build apt-get update
+    docker exec build apt-get install -y cmake lcov $COMPILER
+fi
---
+++ .travis.script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+env
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo ""
+else
+    docker exec build /bin/bash -c "which $CCOMPILE"
+    docker exec build /bin/bash -c "which $CXXCOMPILE"
+    docker exec build /bin/bash -c "$CXXCOMPILE --version"
+    docker exec build /bin/bash -c "cd /project/environment; CC=$CCOMPILE CXX=$CXXCOMPILE cmake ."
+    docker exec build /bin/bash -c "cd /project/environment; CC=$CCOMPILE CXX=$CXXCOMPILE make"
+fi
