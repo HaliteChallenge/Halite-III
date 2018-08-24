7addae7c3db8474b6059fe0ee82d6a249476c3dd
David Li <David.M.Li@TwoSigma.com>
2018-08-24 17:07:57 -0400

    Install CUDA-9.0

--- admin/setup_gpu_worker_image.sh
+++ admin/setup_gpu_worker_image.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "This script should be run after the base worker image setup script."
 
 echo "Installing CUDA and cudnn."
 
 curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-wget -O libcudnn.deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn6_6.0.21-1+cuda8.0_amd64.deb
-wget -O libcudnn-dev.deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn6-dev_6.0.21-1+cuda8.0_amd64.deb
 
 sudo dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt-get update
-sudo apt-get remove libglx-mesa0
-sudo apt-get install nvidia-396 cuda -y
-sudo dpkg -i libcudnn.deb
-sudo dpkg -i libcudnn-dev.deb
+sudo apt-get -qq update
+# Without force-overwrite nvidia-396 conflicts with libglx0-mesa
+sudo apt-get -q -o Dpkg::Options::="--force-overwrite" install nvidia-396 cuda cuda-9-0 -y
 
 echo 'export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}' >> ~/.profile
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.profile
@@ -27,7 +25,7 @@ sudo pip3 install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp36-cp
 
 # Print out installed versions of packages
 PACKAGES="cuda libcupti-dev"
-PYTHON_PACKAGES="tensorflow-gpu"
+PYTHON_PACKAGES="tensorflow-gpu torch"
 
 echo "Packages"
 for package in ${PACKAGES}; do
