311a01a8c92d5156a057b4605b091601bf843572
David Li <David.M.Li@TwoSigma.com>
2018-08-27 09:21:52 -0400

    Install proper versions of CUDA/libcudnn

--- admin/setup_gpu_worker_image.sh
+++ admin/setup_gpu_worker_image.sh
@@ -6,15 +6,23 @@ echo "This script should be run after the base worker image setup script."
 
 echo "Installing CUDA and cudnn."
 
+# Newer repo doesn't have cuda-9-0 which our version of TensorFlow depends upon.
 curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 
 sudo dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get -qq update
 # Without force-overwrite nvidia-396 conflicts with libglx0-mesa
-sudo apt-get -q -o Dpkg::Options::="--force-overwrite" install nvidia-396 cuda cuda-9-0 -y
+sudo apt-get -q -o Dpkg::Options::="--force-overwrite" install nvidia-396 cuda-9-0 -y
 
-echo 'export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}' >> ~/.profile
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.profile
+# Install libcudnn separately (making sure it matches the cuda version)
+wget -O libcudnn.deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.2.1.38-1+cuda9.0_amd64.deb
+wget -O libcudnn-dev.deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.2.1.38-1+cuda9.0_amd64.deb
+
+sudo dpkg -i ./libcudnn.deb
+sudo dpkg -i ./libcudnn-dev.deb
+
+echo 'export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}' >> ~/.profile
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.profile
 source ~/.profile
 
 sudo apt-get install -y libcupti-dev libcupti9.1
@@ -24,7 +32,7 @@ sudo pip3 install tensorflow-gpu
 sudo pip3 install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp36-cp36m-manylinux1_x86_64.whl
 
 # Print out installed versions of packages
-PACKAGES="cuda libcupti-dev"
+PACKAGES="libcupti-dev libcudnn"
 PYTHON_PACKAGES="tensorflow-gpu torch"
 
 echo "Packages"
