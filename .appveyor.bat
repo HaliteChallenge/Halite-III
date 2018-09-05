9bd6bda1e9fecce07d94cde6d253f908add32e1d
David Li <David.M.Li@TwoSigma.com>
2018-09-05 09:35:51 -0400

    Use 7z instead of Compress-Archive on AppVeyor

--- .appveyor.bat
+++ .appveyor.bat
@@ -1,11 +1,16 @@
+@echo on
+
 cd C:\Halite\game_engine
 
 setlocal
+echo "Building game engine for AMD64."
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-.\make.bat
+cmake -H. -Bbuild
+cmake --build build --config Release
 endlocal
 
 setlocal
+echo "Building game engine for x86."
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 cmake -H. -Bbuild32
 cmake --build build32 --config Release
