6a7114ef391578f34be736c609fcd36596b384d2
Snaar <snaar@snaar.net>
2018-09-10 13:19:25 -0400

    cpp-kit: add couple missing functions

--- starter_kits/C++/hlt/game_map.hpp
+++ starter_kits/C++/hlt/game_map.hpp
@@ -27,6 +27,20 @@ namespace hlt {
             return at(entity->position);
         }
 
+        int calculate_distance(const Position& source, const Position& target) {
+            int dx = std::abs(source.x - target.x);
+            int dy = std::abs(source.y - target.y);
+
+            int wrapped_dx = std::min(dx, width - dx);
+            int wrapped_dy = std::min(dy, height - dy);
+
+            return wrapped_dx + wrapped_dy;
+        }
+
+        Position normalize(const Position& position) {
+            return { position.x % width, position.y % height };
+        }
+
         void _update();
         static std::unique_ptr<GameMap> _generate();
     };
