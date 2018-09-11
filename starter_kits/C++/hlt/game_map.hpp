ba14daecbc451bd45440e389eab1fb4a1c924286
Snaar <snaar@snaar.net>
2018-09-10 20:11:59 -0400

    cpp-kit: adding naive_navigate

--- starter_kits/C++/hlt/game_map.hpp
+++ starter_kits/C++/hlt/game_map.hpp
@@ -28,17 +28,52 @@ namespace hlt {
         }
 
         int calculate_distance(const Position& source, const Position& target) {
-            int dx = std::abs(source.x - target.x);
-            int dy = std::abs(source.y - target.y);
+            const int dx = std::abs(source.x - target.x);
+            const int dy = std::abs(source.y - target.y);
 
-            int wrapped_dx = std::min(dx, width - dx);
-            int wrapped_dy = std::min(dy, height - dy);
+            const int toroidal_dx = std::min(dx, width - dx);
+            const int toroidal_dy = std::min(dy, height - dy);
 
-            return wrapped_dx + wrapped_dy;
+            return toroidal_dx + toroidal_dy;
         }
 
         Position normalize(const Position& position) {
             return { position.x % width, position.y % height };
+        }
+
+        std::vector<Direction> get_unsafe_moves(const Position& source, const Position& destination) {
+            std::vector<Direction> possible_moves;
+
+            const int dx = std::abs(source.x - destination.x);
+            const int dy = std::abs(source.y - destination.y);
+            const int wrapped_dx = width - dx;
+            const int wrapped_dy = height - dy;
+
+            if (source.x < destination.x) {
+                possible_moves.push_back(dx > wrapped_dx ? Direction::WEST : Direction::EAST);
+            } else if (source.x > destination.x) {
+                possible_moves.push_back(dx < wrapped_dx ? Direction::WEST : Direction::EAST);
+            }
+
+            if (source.y < destination.y) {
+                possible_moves.push_back(dy > wrapped_dy ? Direction::NORTH : Direction::SOUTH);
+            } else if (source.y > destination.y) {
+                possible_moves.push_back(dy < wrapped_dy ? Direction::NORTH : Direction::SOUTH);
+            }
+
+            return possible_moves;
+        }
+
+        Direction naive_navigate(std::shared_ptr<Ship> ship, const Position& destination) {
+            for (auto direction : get_unsafe_moves(ship->position, destination)) {
+                Position target_pos = ship->position.directional_offset(direction);
+                if (!at(target_pos)->is_occupied()) {
+                    at(target_pos)->mark_unsafe(ship);
+                    return direction;
+                }
+            }
+            at(ship)->mark_unsafe(ship);
+            return Direction::STILL;
         }
 
         void _update();
