684adc9d22b16f1ed05208b714e4be900a4d269b
Snaar <snaar@snaar.net>
2018-09-11 02:44:44 -0400

    cpp-kit: dropoff is not a ship

--- starter_kits/C++/hlt/dropoff.cpp
+++ starter_kits/C++/hlt/dropoff.cpp
@@ -2,10 +2,10 @@
 #include "input.hpp"
 
 std::shared_ptr<hlt::Dropoff> hlt::Dropoff::_generate(hlt::PlayerId player_id) {
-    hlt::EntityId ship_id;
+    hlt::EntityId dropoff_id;
     int x;
     int y;
-    hlt::get_sstream() >> ship_id >> x >> y;
+    hlt::get_sstream() >> dropoff_id >> x >> y;
 
-    return std::make_shared<hlt::Dropoff>(player_id, ship_id, x, y);
+    return std::make_shared<hlt::Dropoff>(player_id, dropoff_id, x, y);
 }
