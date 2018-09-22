b426938286e653cc88688078efc1eecee17876cd
whatelsie <elsie@halite.io>
2018-09-22 15:27:47 -0400

    Replaced missing </p> and added link to home.

--- website/javascript/templates/404.vue
+++ website/javascript/templates/404.vue
@@ -2,15 +2,14 @@
   <div class="home-container view404-container" style="text-align: center;">
       <div>
         <div>
-          <p class="d1 c-org font-headline" style="font-size: 30px;"> Are you lost at sea?
+          <p class="d1 c-org font-headline" style="font-size: 30px;"> Are you lost at sea?</p>
         </div>
         <div>
-          <br/><br/>
+          <br>
           <img src="/assets/images/home-map.png" alt="Map">
-          <br/>
         </div>
-        <div>
-          <p class="d1 c-org font-headline" style="padding-top:15px;"> Maybe you're looking for <a href="https://2017.halite.io/" target="_blank">Halite II</a>?</p>
+        <div> 
+          <p class="d1 c-org font-headline"> Return <a href="/" target="_blank">home</a> or navigate to <a href="https://2017.halite.io/" target="_blank">Halite II</a>.</p>
         </div>
       </div>
   </div>
