7a3c404e04378fe0a609d6f34d63660349f99cf2
David Li <David.M.Li@TwoSigma.com>
2018-09-26 13:22:10 -0400

    Build with VS2017 on AppVeyor

--- .appveyor.bat
+++ .appveyor.bat
@@ -4,14 +4,14 @@ cd C:\Halite\game_engine
 
 setlocal
 echo "Building game engine for AMD64."
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -H. -Bbuild
 cmake --build build --config Release
 endlocal
 
 setlocal
 echo "Building game engine for x86."
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 cmake -H. -Bbuild32
 cmake --build build32 --config Release
 endlocal
