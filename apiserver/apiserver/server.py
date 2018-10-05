a73ef23acee9b1f32dcc439f938cc2d487c36231
David Li <David.M.Li@TwoSigma.com>
2018-10-05 17:03:26 -0400

    Redirect HTTP to HTTPS (fixes #365)

--- apiserver/apiserver/server.py
+++ apiserver/apiserver/server.py
@@ -2,6 +2,10 @@ from . import app, setup_logging
 from . import login
 from . import web
 
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+import flask
 import flask_oauthlib.client
 from flask_compress import Compress
 
@@ -15,3 +19,10 @@ setup_logging("login.log", login.login_log)
 app.register_blueprint(login.oauth_login, url_prefix="/v1/login")
 app.register_blueprint(login.oauth_logout, url_prefix="/v1/logout")
 app.register_blueprint(web.web_api, url_prefix="/v1/api")
+
+@app.before_request
+def force_https():
+    if not flask.request.is_secure and flask.request.endpoint and \
+            not flask.request.url.startswith('http://localhost') and \
+            flask.request.endpoint != 'health_check':
+        return flask.redirect(flask.request.url.replace('http://', 'https://'))
