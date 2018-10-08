a54af6d411debb93dc5283c169c1f283fa1c6e72
Scott Lynch <s.a.lynch@gmail.com>
2018-10-07 23:11:40 -0500

    Bugfix #467 - Catch EOFError, clean exit.

--- starter_kits/Python3/MyBot.py
+++ starter_kits/Python3/MyBot.py
@@ -33,31 +33,36 @@ logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 """ <<<Game Loop>>> """
 
 while True:
-    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
-    #   running update_frame().
-    game.update_frame()
-    # You extract player metadata and the updated map metadata here for convenience.
-    me = game.me
-    game_map = game.game_map
+    try:
+        # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
+        #   running update_frame().
+        game.update_frame()
+        # You extract player metadata and the updated map metadata here for convenience.
+        me = game.me
+        game_map = game.game_map
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
-    command_queue = []
+        # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+        #   end of the turn.
+        command_queue = []
 
-    for ship in me.get_ships():
-        # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-        #   Else, collect halite.
-        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-            command_queue.append(
-                ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
-        else:
-            command_queue.append(ship.stay_still())
+        for ship in me.get_ships():
+            # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
+            #   Else, collect halite.
+            if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
+                command_queue.append(
+                    ship.move(
+                        random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+            else:
+                command_queue.append(ship.stay_still())
 
-    # If the game is in the first 200 turns and you have enough halite, spawn a ship.
-    # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
-        command_queue.append(me.shipyard.spawn())
+        # If the game is in the first 200 turns and you have enough halite, spawn a ship.
+        # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
+        if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+            command_queue.append(me.shipyard.spawn())
 
-    # Send your moves back to the game environment, ending this turn.
-    game.end_turn(command_queue)
+        # Send your moves back to the game environment, ending this turn.
+        game.end_turn(command_queue)
+    except EOFError:
+        logging.info("Halite server terminated. Exiting")
+        break
+
