2b7a43ebdd77db547b5bb59c8240cdeccaccdbcf
David Li <David.M.Li@TwoSigma.com>
2018-10-10 08:55:34 -0400

    Link tier badge to proper place (fixes #538)

--- website/javascript/templates/TierPopover.vue
+++ website/javascript/templates/TierPopover.vue
@@ -1,6 +1,18 @@
 <template>
-  <a href="#" :title="`${badge} Tier (${percentage}% of players)`">
+  <a
+    :title="`${badge} Tier (${percentage}% of players)`"
+    @click="open = !open"
+  >
     <span class="tier-clickable" :class="tier"></span>
+    <transition name="fadeIn">
+      <div v-if="open" class="tier-popup">
+        <header>
+          <h4><span :class="tier"></span> {{badge}} Tier</h4>
+          <span class="close-btn icon-remove" />
+        </header>
+        <p>{{percentage}}% of players achieve this badge.</p>
+      </div>
+    </transition>
   </a>
 </template>
 
@@ -18,19 +30,24 @@
       this.index = index
       this.badge = badge
       this.percentage = percentages[index - 1]
-  },
+    },
     data: function () {
       return {
         index: '',
         badge: '',
-        percentage: ''
+        percentage: '',
+        open: false,
       }
-    }
+    },
   }
 </script>
 
-<style lang="scss">
-  .tier-clickable {
-    cursor: pointer;
-  }
+<style lang="scss" scoped>
+    a {
+      position: relative;
+    }
+
+    a:hover {
+      text-decoration: none;
+    }
 </style>
