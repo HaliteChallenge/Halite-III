d46eec8ff18c51d83531c8486d5fa36c97614b1c
David Li <David.M.Li@TwoSigma.com>
2018-10-10 18:20:15 -0400

    Optimize org leaderboard somewhat

--- apiserver/apiserver/web/leaderboard.py
+++ apiserver/apiserver/web/leaderboard.py
@@ -254,11 +254,13 @@ def organization_leaderboard():
         model.organizations.c.id.label("organization_id"),
         model.organizations.c.organization_name,
         model.organizations.c.kind.label("organization_kind"),
-        sqlalchemy.sql.func.count().label("weekly_games_won"),
+        sqlalchemy.sql.func.count(
+            sqlalchemy.case([(model.game_participants.c.rank == 1, 1)])
+        ).label("weekly_games_won"),
+        sqlalchemy.sql.func.count().label("weekly_games_played"),
     ]).select_from(model.games.join(
         model.game_participants,
         (model.games.c.id == model.game_participants.c.game_id) &
-        (model.game_participants.c.rank == 1) &
         (model.games.c.time_played >= sqlalchemy.sql.func.current_timestamp() - sqlalchemy.sql.text("interval '7' day"))
     ).join(
         model.users,
@@ -272,28 +274,6 @@ def organization_leaderboard():
         model.organizations.c.kind,
     ).alias("organization_wins")
 
-    organization_games_inner = sqlalchemy.sql.select([
-        model.organizations.c.id.label("organization_id"),
-        model.games.c.id,
-    ], distinct=True).select_from(model.games.join(
-        model.game_participants,
-        (model.games.c.id == model.game_participants.c.game_id) &
-        (model.games.c.time_played >= sqlalchemy.sql.func.current_timestamp() - sqlalchemy.sql.text("interval '7' day"))
-    ).join(
-        model.users,
-        model.game_participants.c.user_id == model.users.c.id
-    ).join(
-        model.organizations,
-        model.users.c.organization_id == model.organizations.c.id
-    )).alias("organization_games_inner")
-
-    organization_games = sqlalchemy.sql.select([
-        organization_games_inner.c.organization_id,
-        sqlalchemy.sql.func.count().label("weekly_games_played"),
-    ]).select_from(organization_games_inner).group_by(
-        organization_games_inner.c.organization_id,
-    ).alias("organization_games")
-
     organization_players = sqlalchemy.sql.select([
         model.users.c.organization_id,
         sqlalchemy.sql.func.count().label("num_players"),
@@ -306,15 +286,12 @@ def organization_leaderboard():
         organization_wins.c.organization_name,
         organization_wins.c.organization_kind,
         organization_wins.c.weekly_games_won,
-        organization_games.c.weekly_games_played,
+        organization_wins.c.weekly_games_played,
         organization_players.c.num_players,
         sqlalchemy.sql.func.rank().over(
             order_by=organization_wins.c.weekly_games_won.desc()
         ).label("organization_rank"),
     ]).select_from(organization_wins.join(
-        organization_games,
-        organization_wins.c.organization_id == organization_games.c.organization_id
-    ).join(
         organization_players,
         organization_wins.c.organization_id == organization_players.c.organization_id
     )).reduce_columns().alias("organization_ranks")
