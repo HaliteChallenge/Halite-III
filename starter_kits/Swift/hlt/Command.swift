718643c59b8c6b086e70b79aad28e3eccad78750
Chris Downie <cdownie@gmail.com>
2018-10-24 17:37:44 -0600

    Stub implementation of all objects.

--- starter_kits/Swift/hlt/Command.swift
+++ starter_kits/Swift/hlt/Command.swift
@@ -1,27 +1,62 @@
 import Foundation
-
-enum Direction: String {
+enum Command: String {
     case north = "n"
-    case west = "w"
     case south = "s"
     case east = "e"
-
-    case still = "still"
+    case west = "w"
+    case stayStill = "o"
+    case generate = "g"
+    case construct = "c"
+    case move = "m"
 }
 
-enum Command {
-    case generate
-    case construct
-    case move(direction: Direction)
+enum Direction {
+    case north, west, south, east
+    case still
     
-    static func string(for command: Command) -> String {
-        switch command {
-        case .generate:
-            return "g"
-        case .construct:
-            return "c"
-        case .move(let direction):
-            return "m\(direction.rawValue)"
+    /// Returns an array of all cardinal directions
+    ///
+    /// - Returns: An array of all cardinal directions
+    static func getAllCardinals() -> [Direction] {
+        return [.north, .west, .south, .east]
+    }
+    
+    /// Returns a letter command from a direction
+    ///
+    /// - Parameter direction: The direction to convert to a
+    /// - Returns: A command equivalent to the given direction
+    static func convert(_ direction: Direction) -> Command {
+        switch direction {
+        case .north:
+            return .north
+        case .south:
+            return .south
+        case .east:
+            return .east
+        case .west:
+            return .west
+        case .still:
+            return .stayStill
+        }
+    }
+    
+    /// Returns the opposite cardinal direction given a direction
+    ///
+    /// - Parameter direction: The input direction
+    /// - Returns: The opposite direction
+    static func invert(_ direction: Direction) -> Direction {
+        switch direction {
+        case .north:
+            return .south
+        case .south:
+            return .north
+        case .east:
+            return .west
+        case .west:
+            return .east
+        case .still:
+            return .still
         }
     }
 }
+
--- starter_kits/Swift/hlt/Player.swift
+++ starter_kits/Swift/hlt/Player.swift
@@ -3,6 +3,35 @@ import Foundation
 struct Position {
     let x: Int
     let y: Int
+    
+    /// Returns a new position based on moving one unit in the given direction from the given position.
+    ///
+    /// This method takes a direction such as Direction.west or an equivalent tuple such as (0, -1), but will not
+    /// work with commands such as "w".
+    ///
+    /// - Parameter direction: The direction to move this position
+    /// - Returns: The position moved one unit from the given direction
+    func directionalOffset(_ direction: Direction) -> Position {
+        // TODO
+        return Position(x:0, y: 0)
+    }
+    
+    /// This is a convenience method for directionalOffset(:) that takes a tuple instead of a direction.
+    ///
+    /// - Parameter tuple: An (x,y) tuple to offset the current position. It should represent one square in any
+    ///   cardinal direction
+    /// - Returns: The position moved one unit from the given position.
+    func directionalOffset(tuple: (Int, Int)) -> Position {
+        // TODO
+        return Position(x: tuple.0, y: tuple.1)
+    }
+    
+    /// Returns a list of all positions around the given position in each cardinal direction.
+    ///
+    /// - Returns: A list of all positions around the given position in each cardinal direction.
+    func getSurroundingCardinals() -> [Position] {
+        return []
+    }
 }
 
 struct Player {
@@ -84,7 +113,7 @@ struct Ship {
     ///
     /// - Returns: An engine command to keep this ship where it is and collect halite.
     func stayStill() -> Command {
-        return .move(direction: .north)
+        return .stayStill
     }
     
     /// Returns an engine command to move this ship in a direction without checking for collisions.
@@ -96,7 +125,7 @@ struct Ship {
     /// - Parameter direction: The direction to move
     /// - Returns: An engine command to move this ship in a direction without checking for collisions.
     func move(direction: Direction) -> Command {
-        return .move(direction: direction)
+        return Direction.convert(direction)
     }
 }
 
