7b7deb7ffed0dde878c1348783a063d657d6dbef
snaar <snaar@snaar.net>
2018-10-25 11:52:39 -0400

    don't close the child process handle right away, we need it later to terminate

--- game_engine/networking/win32/WinConnection.cpp
+++ game_engine/networking/win32/WinConnection.cpp
@@ -85,7 +85,6 @@ WinConnection::WinConnection(const std::string &command, NetworkingConfig config
         CloseHandle(write[PIPE_TAIL]);
         throw NetworkingError("Could not start process");
     }
-    CloseHandle(piProcInfo.hProcess);
     CloseHandle(piProcInfo.hThread);
     read_pipe = read[PIPE_HEAD];
     write_pipe = write[PIPE_TAIL];
