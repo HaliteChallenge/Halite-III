f73affce73820c179005176c34ce40cddfdab859
Chris Downie <cdownie@gmail.com>
2018-10-28 08:12:03 -0600

    Rename compile product MyBot in package.

--- starter_kits/Swift/Halite-III/Package.swift
+++ starter_kits/Swift/Halite-III/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Halite-III",
+            name: "MyBot",
             dependencies: [],
             path: "Halite-III"),
     ]
