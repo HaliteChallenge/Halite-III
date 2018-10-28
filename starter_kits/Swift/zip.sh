1fb67603adc56fe699873146a60a74a22a61cfbc
Chris Downie <cdownie@gmail.com>
2018-10-28 06:55:21 -0600

    Ignore build folder when zipping.

--- starter_kits/Swift/zip.sh
+++ starter_kits/Swift/zip.sh
@@ -3,5 +3,5 @@
 set -e
 cp -r Halite-III/Halite-III Files
 touch Files/MyBot.swift
-zip -9 -r --exclude=*DS_Store* MyBot.zip Files/
+zip -9 -r --exclude=*DS_Store* --exclude=*.build* MyBot.zip Files/
 rm -r Files/
