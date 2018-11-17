066aad84db823960d766b8e42ea313b6605e36db
Stoyan Atanasov <stoiancho@gmail.com>
2018-11-17 05:14:58 +0100

    Now runs the TypeScript compiler tsc before running the game.

--- starter_kits/TypeScript/run_game.bat
+++ starter_kits/TypeScript/run_game.bat
@@ -1,1 +1,1 @@
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "node .\out\MyBot.js" "node .\out\MyBot.js"
+tsc && halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "node .\out\MyBot.js" "node .\out\MyBot.js"
