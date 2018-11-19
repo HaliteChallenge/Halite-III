869324f2ce8a05e73bfcff83024d3f9dda0e4352
Stoyan Atanasov <stoiancho@gmail.com>
2018-11-19 10:31:01 +0100

    Modernize Player class

--- starter_kits/TypeScript/hlt/Player.ts
+++ starter_kits/TypeScript/hlt/Player.ts
@@ -5,42 +5,34 @@ import { Position } from "./Position";
 
 /** Player object, containing all entities/metadata for the player. */
 export class Player {
-    private _ships = new Map<number, Ship>();
-    private _dropoffs = new Map<number, Dropoff>();
+    private ships = new Map<number, Ship>();
+    private dropoffs = new Map<number, Dropoff>();
     constructor(public id: number, public shipyard: Shipyard, public haliteAmount = 0) {
     }
 
     /** Get a single ship by its ID. */
     getShip(shipId: number) {
-        return this._ships.get(shipId);
+        return this.ships.get(shipId);
     }
 
     /** Get a list of the player's ships. */
     getShips() {
-        const result = [];
-        for (const ship of this._ships.values()) {
-            result.push(ship);
-        }
-        return result;
+        return Array.from(this.ships.values());
     }
 
     /** Get a single dropoff by its ID. */
     getDropoff(dropoffId: number) {
-        return this._dropoffs.get(dropoffId);
+        return this.dropoffs.get(dropoffId);
     }
 
     /** Get a list of the player's dropoffs. */
     getDropoffs() {
-        const result = [];
-        for (const dropoff of this._dropoffs.values()) {
-            result.push(dropoff);
-        }
-        return result;
+        return Array.from(this.dropoffs.values());
     }
 
     /** Check whether a ship with a given ID exists. */
     hasShip(shipId: number) {
-        return this._ships.has(shipId);
+        return this.ships.has(shipId);
     }
 
     /**
@@ -57,19 +49,18 @@ export class Player {
     /**
      * Update the player object for the current turn using input from
      * the game engine.
-     * @private
      */
     async _update(numShips: number, numDropoffs: number, halite: number, getLine: () => Promise<string>) {
         this.haliteAmount = halite;
-        this._ships = new Map();
+        this.ships = new Map();
         for (let i = 0; i < numShips; i++) {
             const { shipId, ship } = await Ship._generate(this.id, getLine);
-            this._ships.set(shipId, ship);
+            this.ships.set(shipId, ship);
         }
-        this._dropoffs = new Map();
+        this.dropoffs = new Map();
         for (let i = 0; i < numDropoffs; i++) {
             const { id, dropoff } = await Dropoff._generate(this.id, getLine);
-            this._dropoffs.set(id, dropoff);
+            this.dropoffs.set(id, dropoff);
         }
     }
 }
