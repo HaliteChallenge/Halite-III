eee6b38172042fa459ffd8ef9d3620fcb77cf859
Stoyan Atanasov <stoiancho@gmail.com>
2018-11-22 11:26:42 +0100

    Moved out server communication.

--- starter_kits/TypeScript/hlt/Dropoff.ts
+++ starter_kits/TypeScript/hlt/Dropoff.ts
@@ -1,15 +1,5 @@
-import { Position } from "./Position";
 import { Entity } from './entity';
 
 /** Represents a dropoff. */
 export class Dropoff extends Entity {
-    /**
-     * Create a Dropoff for a specific player from the engine input.
-     */
-    static async _generate(playerId: number, getLine: () => Promise<string>) {
-        const [id, xPos, yPos] = (await getLine())
-            .split(/\s+/)
-            .map(x => parseInt(x, 10));
-        return { id, dropoff: new Dropoff(playerId, id, new Position(xPos, yPos)) };
-    }
 }
